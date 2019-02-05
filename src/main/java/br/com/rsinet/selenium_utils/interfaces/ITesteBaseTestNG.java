6dfde818e19a014fbab954d7f9176883fb9df4d8
Denis Luna Borges da Silva <p000densilva@prservicos.com.br>
2019-02-05 17:07:43 -0200

    Remoção da classe TesteBaseWebDriver. Adição da interface ITesteBaseTestNG. Atualização para a versão 1.2.0

---
+++ src/main/java/br/com/rsinet/selenium_utils/interfaces/ITesteBaseTestNG.java
@@ -0,0 +1,25 @@
+package br.com.rsinet.selenium_utils.interfaces;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public interface ITesteBaseTestNG {
+
+	/**
+	 * Método que executa a inicialização do teste baseado em navegação
+	 * {@link WebDriver}.
+	 */
+	@BeforeClass
+	void start();
+
+	/**
+	 * Método que executa a finalizaçao do teste baseado em navegação
+	 * {@link WebDriver}.
+	 * 
+	 * @param result objeto do tipo {@link ITestResult} com o resultado do teste.
+	 */
+	@AfterClass
+	void finish(ITestResult result);
+}
